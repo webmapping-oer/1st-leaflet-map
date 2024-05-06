53130099dc8d37556339fded21b3f5e1eb5faa0f
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-06 15:30:15 +0200

    Hintergrundkarte der OpenStreetMap mit Quelle hinzugefügt

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ let map = L.map("map");
 
 // auf den Stephansdom blicken
 map.setView([48.208493, 16.373118], 12);
+
+// Hintergrundkarte der OpenStreetMap hinzufügen
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`
+}).addTo(map);
