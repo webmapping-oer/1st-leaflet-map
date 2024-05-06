eed8285ab0fdfeeb79fe56301b71d6e9cd177c72
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-06 15:32:24 +0200

    Popup an den Marker gehängt und geöffnet

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // Marker für den Stephansdom hinzufügen
 let marker = L.marker([48.208493, 16.373118]).addTo(map);
+
+// Popup zum Marker hinzufügen und öffnen
+marker.bindPopup(`
+    <h4>Stephansdom</h4>
+`).openPopup();
